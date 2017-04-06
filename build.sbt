f2c1eb7fa99a99bc93e0abdc4dcad56cf0b2623d
Julián Vanegas Piedrahita <julianvanegas357@gmail.com>
2017-04-06 16:36:29 -0500

    Added Spark dependency

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.11.9"
 
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.1"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.1.0"
--- src/main/scala/co/edu/eia/metagenomics/ClusteringApp.scala
+++ src/main/scala/co/edu/eia/metagenomics/ClusteringApp.scala
@@ -1,9 +1,18 @@
 package co.edu.eia.metagenomics
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
 object ClusteringApp {
-
-  def main(args: Array[String]): Unit = {
-    println("Hello world")
+  def main(args: Array[String]) {
+    val logFile = "test.txt" // Should be some file on your system
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    sc.stop()
   }
-
 }
