a62899b7799af9d65e31819dffd169b07790cd49
Julián Vanegas Piedrahita <julianvanegas357@gmail.com>
2017-04-26 16:28:51 -0500

    Modified KMeansExample and added winutils.exe file.
    Added a seed to the kmeans|| initialization and printed resulting cluster centers.

--- src/main/scala/co/edu/eia/metagenomics/mllib/KMeansExample.scala
+++ src/main/scala/co/edu/eia/metagenomics/mllib/KMeansExample.scala
@@ -6,19 +6,22 @@ import org.apache.spark.mllib.linalg.Vectors
 
 object KMeansExample {
   def main(args: Array[String]) {
+    //TODO: This fails if the winutils.exe is inside the project. How to handle this file?
+    System.setProperty("hadoop.home.dir", "C:\\Program Files\\hadoop")
     val conf = new SparkConf().setAppName("KMeansExample").setMaster("local")
     val sc = new SparkContext(conf)
 
     val data = sc.textFile("iris.txt")
     val parsedData = data.map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
 
-    val numClusters = 2
-    val numIterations = 100
-    val clusters = KMeans.train(parsedData, numClusters, numIterations)
+    val numClusters = 3
+    val numIterations = 500
+    val clusters = KMeans.train(data = parsedData, k = numClusters, maxIterations = numIterations, initializationMode = "kmeans||", seed = 10)
 
     val WSSSE = clusters.computeCost(parsedData)
+    val centers = clusters.clusterCenters.deep.mkString(" ")
     println(s"Within Set Sum of Squared Errors = $WSSSE")
-
+    println(s"Centers = $centers")
     clusters.save(sc, "target/co/edu/eia/metagenomics/KMeansExample/KMeansModel")
 
     sc.stop()
